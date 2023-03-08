c4e3be62d31d76382162442fc3afa251345bfc4a
Abbasz04 <115218115+Abbasz04@users.noreply.github.com>
2023-03-08 16:04:36 +0700

    Add files via upload

---
+++ proses_registrasi.php
@@ -0,0 +1,23 @@
+<?php
+$domisili = ["jakarta","bogor","depok","tangerang","bekasi"];
+
+
+$program_studi = [
+    "si"=> "sistem informasi",
+    "Ti"=> "Teknik informatika",
+    "BD"=> "Bisnis digital"
+];
+
+$skills = [
+    "html"=>10,
+    "css"=>10,
+    "javascipt"=>20,
+    "php"=>30,
+    "phyton"=>30,
+];
+
+
+
+
+
+?>
